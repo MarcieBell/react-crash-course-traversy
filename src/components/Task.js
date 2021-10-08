d80e649917067d2641c476b738c2fb3a4f24e3d7
asxyzp <asxyzp@gmail.com>
2021-10-08 22:51:49 +0530

    + Adding onToggle prop on double click for Task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -7,12 +7,11 @@ import { FaTimesCircle } from 'react-icons/fa';
  * @param {*} {properties} : PROPERTIES OF THE TASK COMPONENT
  * @returns 
  */
-const Task = ({ properties, onDelete }) => {
+const Task = ({ properties, onDelete, onToggle }) => {
 
-
-    //
+    //<Task/>(JSX)
     return (
-        <div className="task rounded p-2 m-2 d-flex justify-content-between align-items-center">
+        <div onDoubleClick={()=>{ onToggle(properties.id) }} className={`task rounded p-2 m-2 d-flex justify-content-between align-items-center ${properties.reminder?"border-5 border-start border-danger":""}`}>
             <div>
                 <div className="fw-bolder">{properties.text}</div>
                 <div>{properties.day}</div>
