db1f50070c356f2194d104675fddc0bcfeeb3e0b
asxyzp <asxyzp@gmail.com>
2021-10-08 23:01:25 +0530

    + Methods for deleting & toggling task + States for containing task data & task form state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
-import React from "react";
 import "./style/style.css";
+import Tasks from "./components/Tasks";
+import React, { useState } from 'react';
 import Header from "./components/Header";
+import AddTask from "./components/AddTask";
 
 function App() {
-  return (
-    <div className="app border border-dark">
-      <Header/>
-    </div>
-  );
+
+    //TASKS
+    const [tasks, setTasks] = useState([]);                           //SETS TASKS STATE
+    const [taskFormState, setTaskFormState] = useState(false);      //SETS TASK FORM STATE
+
+    /**
+     * FUNCTION : deleteTask(id)
+     * FUNCTIONALITY : DELETING/REMOVING TASKS FROM THE TASK STATE WITH A PARTICULAR ID  
+     * @param {*} id : ID OF THE TASK WHICH NEEDS TO BE DELETED
+     * @returns : UNDEFINED
+     */
+    const deleteTask = (id) => {
+        tasks.filter((task) => {
+            setTasks(tasks.filter((task, index, array) => {
+                if(task.id!==id)
+                    return task;
+            }));
+        });
+    };
+
+    /**
+     * FUNCTION : toggleTask(id)
+     * FUNCTIONALITY : TOGGLE THE REMINDER VALUE OF TASKS
+     * @param {*} id : ID OF THE TOGGLED TASK
+     * @returns : UNDEFINED
+     */
+    const toggleTask = (id) =>{
+        setTasks(
+            tasks.map((task) => {
+                if(task.id === id)  task.reminder = !task.reminder;                    
+                return task
+            }
+        ));
+    };
+
+    return (
+        <div className="app border border-dark p-3">
+            <Header taskFormState={taskFormState} setTaskFormState={setTaskFormState}/>
+            {<AddTask tasks={tasks} setTasks={setTasks} />}
+            {
+                tasks.length>0?
+                <Tasks tasks={tasks} setTasks={setTasks} onDelete={deleteTask} onToggle={toggleTask} className="mt-3"/>:
+                <div className="m-2">No tasks to show</div>
+            }
+        </div>
+    );
 }
 
 export default App;
