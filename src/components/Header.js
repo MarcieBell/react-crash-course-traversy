fa83878f68fb85a70ddc3e7e7b05d86cd505623f
asxyzp <asxyzp@gmail.com>
2021-10-14 17:55:43 +0530

    Showing button in header only using when the path is /

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 //IMPORTING PACKAGES
 import React from 'react';
 import PropTypes from 'prop-types';
+import {useLocation} from 'react-router-dom';
 
 /**
  * FUNCTION : Header(props)
@@ -12,12 +13,18 @@ const Header = ({ title, onAdd, showAddForm }) => {
 
     //STYLE OBJECT
     const Style = { fontWeight: "900", };
+
+    //GETTING LOCATION OBJECT
+    const location = useLocation();
     
     //COMPONENT VALUE
     return (
         <div style={Style} className="mx-auto d-flex justify-content-between align-items-center">
             <h1 className="fw-bolder">{title}</h1>
-            <button onClick={onAdd} className="btn btn-dark">{showAddForm?"Close":"Add"}</button>
+            {
+                location.pathname==="/"?
+                <button onClick={onAdd} className="btn btn-dark">{showAddForm?"Close":"Add"}</button>:""
+            }
         </div>
     );
 }
